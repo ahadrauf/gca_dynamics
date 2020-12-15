eb9ee6bf5c655e837300c743751fdeb69646c14c
Ahad Rauf <ahadrauf@gmail.com>
2020-12-15 02:25:04 -0800

    First pullin simulation vs. data!

--- gca.py
+++ gca.py
@@ -17,11 +17,11 @@ class GCA:
     def dx_dt(self, t, x, u):
         t_SOI = self.process.t_SOI
         density = self.process.density
-        m = self.spineA * t_SOI * density
-        m_spring = 2 * self.supportW * self.supportL * t_SOI * density
-        m_eff = m + m_spring / 3
-        m_total = m_eff + self.Nfing * self.process.density_fluid * (self.fingerL ** 2) * (self.process.t_SOI ** 2) / (
-                2 * (self.fingerL + self.process.t_SOI))
+        m = self.spineA*t_SOI*density
+        m_spring = 2*self.supportW*self.supportL*t_SOI*density
+        m_eff = m+m_spring/3
+        m_total = m_eff+self.Nfing*self.process.density_fluid*(self.fingerL**2)*(self.process.t_SOI**2)/(
+                2*(self.fingerL+self.process.t_SOI))
 
         Fes = self.Fes(x, u)
         Fb = self.Fb(x, u)
@@ -29,17 +29,17 @@ class GCA:
         self.add_to_sim_log(['t', 'Fes', 'Fb', 'Fk'], [t, Fes, Fb, Fk])
 
         return np.array([x[1],
-                         (Fes - Fb - Fk) / m_total])
+                         (Fes-Fb-Fk)/m_total])
 
     def Fes(self, x, u):
         x, xdot = self.unzip_state(x)
         V = self.unzip_input(u)
-        return self.Nfing * 0.5 * (V ** 2) * self.process.eps0 * self.process.t_SOI * self.fingerL * (
-                1 / (self.gf - x) ** 2 - 1 / (self.gb + x) ** 2)
+        return self.Nfing*0.5*(V**2)*self.process.eps0*self.process.t_SOI*self.fingerL*(
+                1/(self.gf-x)**2-1/(self.gb+x)**2)
 
     def Fk(self, x, u):
         x, xdot = self.unzip_state(x)
-        return self.k_support * x
+        return self.k_support*x
 
     def Fb(self, x, u):
         x, xdot = self.unzip_state(x)
@@ -51,24 +51,24 @@ class GCA:
 
         S1 = max(fingerL, t_SOI)
         S2 = min(fingerL, t_SOI)
-        beta = lambda eta: 1 - (1 - 0.42) * eta
+        beta = lambda eta: 1-(1-0.42)*eta
 
-        t_SOI_primef = t_SOI + 0.81 * (1 + 0.94 * self.process.mfp / (gf - x)) * (gf - x)
-        t_SOI_primeb = t_SOI + 0.81 * (1 + 0.94 * self.process.mfp / (gb + x)) * (gb + x)
-        bsff = self.process.mu * self.Nfing * S1 * (S2 ** 3) * beta(S2 / S1) * (1 / (gf - x) ** 3)
-        bsfb = self.process.mu * self.Nfing * S1 * (S2 ** 3) * beta(S2 / S1) * (1 / (gb + x) ** 3)
-        bsf_adjf = (4 * (gf - x) ** 3 * fingerW + 2 * self.process.t_ox ** 3 * t_SOI_primef) / (
-                (gf - x) ** 3 * fingerW + 2 * self.process.t_ox ** 3 * t_SOI_primef)
-        bsf_adjb = (4 * (gb + x) ** 3 * fingerW + 2 * self.process.t_ox ** 3 * t_SOI_primeb) / (
-                (gb + x) ** 3 * fingerW + 2 * self.process.t_ox ** 3 * t_SOI_primeb)
-        bsf = bsff * bsf_adjf + bsfb * bsf_adjb
+        t_SOI_primef = t_SOI+0.81*(1+0.94*self.process.mfp/(gf-x))*(gf-x)
+        t_SOI_primeb = t_SOI+0.81*(1+0.94*self.process.mfp/(gb+x))*(gb+x)
+        bsff = self.process.mu*self.Nfing*S1*(S2**3)*beta(S2/S1)*(1/(gf-x)**3)
+        bsfb = self.process.mu*self.Nfing*S1*(S2**3)*beta(S2/S1)*(1/(gb+x)**3)
+        bsf_adjf = (4*(gf-x)**3*fingerW+2*self.process.t_ox**3*t_SOI_primef)/(
+                (gf-x)**3*fingerW+2*self.process.t_ox**3*t_SOI_primef)
+        bsf_adjb = (4*(gb+x)**3*fingerW+2*self.process.t_ox**3*t_SOI_primeb)/(
+                (gb+x)**3*fingerW+2*self.process.t_ox**3*t_SOI_primeb)
+        bsf = bsff*bsf_adjf+bsfb*bsf_adjb
 
         # Couette flow damping
-        bcf = self.process.mu * self.spineA / self.process.t_ox
+        bcf = self.process.mu*self.spineA/self.process.t_ox
 
         # Total damping
-        b = bsf + bcf
-        return b * xdot
+        b = bsf+bcf
+        return b*xdot
 
     def pulled_in(self, t, x):
         x, xdot = self.unzip_state(x)
@@ -89,48 +89,46 @@ class GCA:
             for name, value in csv.reader(data):
                 drawn_dimensions[name] = float(value)
 
-        self.gf = drawn_dimensions["gf"] + 2 * overetch
-        self.gb = drawn_dimensions["gb"] + 2 * overetch
-        self.x_GCA = drawn_dimensions["x_GCA"] + 2 * overetch
-        self.supportW = drawn_dimensions["supportW"] - 2 * overetch
-        self.supportL = drawn_dimensions["supportL"] - overetch
+        self.gf = drawn_dimensions["gf"]+2*overetch
+        self.gb = drawn_dimensions["gb"]+2*overetch
+        self.x_GCA = drawn_dimensions["x_GCA"]+2*overetch
+        self.supportW = drawn_dimensions["supportW"]-2*overetch
+        self.supportL = drawn_dimensions["supportL"]-overetch
         self.Nfing = drawn_dimensions["Nfing"]
-        self.fingerW = drawn_dimensions["fingerW"] - 2 * overetch
-        self.fingerL = drawn_dimensions["fingerL"] - overetch
+        self.fingerW = drawn_dimensions["fingerW"]-2*overetch
+        self.fingerL = drawn_dimensions["fingerL"]-overetch
         self.fingerL_buffer = drawn_dimensions["fingerL_buffer"]
-        self.spineW = drawn_dimensions["spineW"] - 2 * overetch
-        self.spineL = drawn_dimensions["spineL"] - 2 * overetch
-        self.etch_hole_size = drawn_dimensions["etch_hole_size"] + 2 * overetch
-        self.etch_hole_spacing = drawn_dimensions["etch_hole_spacing"] - 2 * overetch
-        self.gapstopW = drawn_dimensions["gapstopW"] - 2 * overetch
-        self.gapstopL_half = drawn_dimensions["gapstopL_half"] - overetch
-        self.anchored_electrodeW = drawn_dimensions["anchored_electrodeW"] - 2 * overetch
-        self.anchored_electrodeL = drawn_dimensions["anchored_electrodeL"] - overetch
+        self.spineW = drawn_dimensions["spineW"]-2*overetch
+        self.spineL = drawn_dimensions["spineL"]-2*overetch
+        self.etch_hole_size = drawn_dimensions["etch_hole_size"]+2*overetch
+        self.etch_hole_spacing = drawn_dimensions["etch_hole_spacing"]-2*overetch
+        self.gapstopW = drawn_dimensions["gapstopW"]-2*overetch
+        self.gapstopL_half = drawn_dimensions["gapstopL_half"]-overetch
+        self.anchored_electrodeW = drawn_dimensions["anchored_electrodeW"]-2*overetch
+        self.anchored_electrodeL = drawn_dimensions["anchored_electrodeL"]-overetch
 
         # Simulating GCAs attached to inchworm motors
         if "armW" in drawn_dimensions:
             self.alpha = drawn_dimensions["alpha"]
-            self.armW = drawn_dimensions["armW"] - 2 * overetch
-            self.armL = drawn_dimensions["armL"] - overetch
+            self.armW = drawn_dimensions["armW"]-2*overetch
+            self.armL = drawn_dimensions["armL"]-overetch
             self.x_impact = drawn_dimensions["x_impact"]
-            self.k_arm = self.process.E * (self.armW ** 3) * self.process.t_SOI / (self.armL ** 3)
+            self.k_arm = self.process.E*(self.armW**3)*self.process.t_SOI/(self.armL**3)
 
+        self.update_dependent_variables()
+
+    def update_dependent_variables(self):
         if not hasattr(self, "k_support"):  # Might be overridden if taking data from papers
-            self.k_support = 2 * self.process.E * (self.supportW ** 3) * self.process.t_SOI / (self.supportL ** 3)
-        if not hasattr(self, "gs"):
-            self.gs = self.gf - self.x_GCA
-        if not hasattr(self, "fingerL_total"):
-            self.fingerL_total = self.fingerL + self.fingerL_buffer
-        if not hasattr(self, "num_etch_holes"):
-            self.num_etch_holes = round((self.spineL - self.etch_hole_spacing - overetch) /
-                                        (self.etch_hole_spacing + self.etch_hole_size))
-        if not hasattr(self, "mainspineA"):
-            self.mainspineA = self.spineW * self.spineL - self.num_etch_holes * (self.etch_hole_size ** 2)
-        if not hasattr(self, "spineA"):
-            self.spineA = self.mainspineA + self.Nfing * self.fingerL_total * self.fingerW + \
-                          2 * self.gapstopW * self.gapstopL_half
-            if "armW" in drawn_dimensions:  # GCA includes arm (attached to inchworm motor)
-                self.spineA += self.armW * self.armL
+            self.k_support = 2*self.process.E*(self.supportW**3)*self.process.t_SOI/(self.supportL**3)
+        self.gs = self.gf-self.x_GCA
+        self.fingerL_total = self.fingerL+self.fingerL_buffer
+        self.num_etch_holes = round((self.spineL-self.etch_hole_spacing-self.process.overetch) /
+                                    (self.etch_hole_spacing+self.etch_hole_size))
+        self.mainspineA = self.spineW*self.spineL-self.num_etch_holes*(self.etch_hole_size**2)
+        self.spineA = self.mainspineA+self.Nfing*self.fingerL_total*self.fingerW+ \
+                      2*self.gapstopW*self.gapstopL_half
+        if hasattr(self, "armW"):  # GCA includes arm (attached to inchworm motor)
+            self.spineA += self.armW*self.armL
 
     def add_to_sim_log(self, names, values):
         for name, value in zip(names, values):
---
+++ sim_gca_V_fingerL_pullin_pullout.py
@@ -0,0 +1,107 @@
+from assembly import AssemblyGCA
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+
+
+def setup_model():
+    model = AssemblyGCA()
+    # model.gca.x0 = np.array([0, 0])
+    model.gca.terminate_simulation = model.gca.pulled_in
+    return model
+
+
+def setup_inputs(**kwargs):
+    V = kwargs["V"]
+    return lambda t, x: np.array([V])
+
+
+def sim_gca(model, u, t_span, verbose=False):
+    f = lambda t, x: model.dx_dt(t, x, u, verbose=verbose)
+    x0 = model.x0()
+    terminate_simulation = lambda t, x: model.terminate_simulation(t, x)
+    terminate_simulation.terminal = True
+
+    sol = solve_ivp(f, t_span, x0, events=[terminate_simulation], dense_output=True, max_step=0.5e-6)
+    return sol
+
+
+def setup_plot(len_x, len_y, plt_title=None, x_label="", y_label=""):
+    fig = plt.figure()
+    if plt_title is not None:
+        fig.suptitle(plt_title)
+
+    fig.text(0.5, 0.04, x_label, ha='center')
+    fig.text(0.04, 0.5, y_label, va='center', rotation='vertical')
+    return fig
+
+
+def plot_data(fig, pullin_V, pullin_avg, pullin_std, release_V, release_avg, release_std, labels):
+    nx, ny = 3, 3
+    for idx in range(nx):
+        for idy in range(ny):
+            i = nx*idy+idx
+            ax = plt.subplot(nx, ny, i+1)
+            plt.errorbar(pullin_V[i], pullin_avg[i], pullin_std[i], fmt='b.', capsize=5)
+            # plt.errorbar(release_V[i], release_avg[i], release_std[i], fmt='r.', capsize=5)
+            ax.text(0.8*ax.get_xlim()[-1], 0.85*ax.get_ylim()[-1], labels[i])
+            # ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+
+
+if __name__ == "__main__":
+    model = setup_model()
+    t_span = [0, 100e-6]
+
+    data = loadmat("data/20180208_fawn_gca_V_fingerL_pullin_release.mat")
+    fingerL_values = np.ndarray.flatten(data["LARR"])*1e-6  # Length = 9
+
+    V_values = np.arange(20, 100, 5)
+    fig = setup_plot(3, 3, x_label="Voltage (V)", y_label="Time (us)")
+
+    pullin_V = []
+    pullin_avg = []
+    pullin_std = []
+    release_V = []
+    release_avg = []
+    release_std = []
+    labels = []
+    for i in range(1, len(fingerL_values)+1):
+        pullin_V.append(np.ndarray.flatten(data["V{}_Arr1".format(i)]))
+        pullin_avg.append(np.ndarray.flatten(data["tmeas{}_Arr1".format(i)]))
+        pullin_std.append(np.ndarray.flatten(data["err{}_Arr1".format(i)]))
+        release_V.append(np.ndarray.flatten(data["V{}_Arr1_r".format(i)]))
+        release_avg.append(np.ndarray.flatten(data["tmeas{}_Arr1_r".format(i)]))
+        release_std.append(np.ndarray.flatten(data["err{}_Arr1_r".format(i)]))
+        labels.append("L={}um".format(fingerL_values[i-1]*1e6))
+
+    plot_data(fig, pullin_V, pullin_avg, pullin_std, release_V, release_avg, release_std, labels)
+
+    nx, ny = 3, 3
+
+    for idy in range(len(fingerL_values)):
+        fingerL = fingerL_values[idy]
+        model.gca.fingerL = fingerL-model.gca.process.overetch
+        model.gca.update_dependent_variables()
+
+        V_converged = []
+        times_converged = []
+
+        V_test = np.sort(np.append(V_values, [pullin_V[idy], pullin_V[idy] + 0.2]))  # Add some extra values to test
+        # (adds a lot of compute time, since failed simulations take time)
+        for V in V_test:
+            u = setup_inputs(V=V)
+            sol = sim_gca(model, u, t_span)
+
+            if len(sol.t_events[0]) > 0:
+                V_converged.append(V)
+                times_converged.append(sol.t_events[0][0] * 1e6)  # us conversion
+        print(times_converged)
+
+        ax = plt.subplot(nx, ny, idy + 1)
+        plt.plot(V_converged, times_converged)
+        # ax.text(0.8*ax.get_xlim()[-1], 0.8*ax.get_ylim()[-1], "w={}um\nL={}um".format(fingerW*1e6, fingerL*1e6))
+        # ax.set_aspect(1.0/ax.get_data_ratio(), adjustable='box')
+
+    plt.tight_layout()
+    plt.show()
