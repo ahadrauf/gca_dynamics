49a705c286c45b483bdbe21e86ed181f2870f426
Ahad Rauf <ahadrauf@gmail.com>
2023-06-24 18:11:44 -0700

    added higher resolution vacuum simulations for pullin/release

---
+++ scripts/plot_data_file_vacuum.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from datetime import datetime
+
+# plt.rc('font', size=18)  # controls default text size
+plt.rc('axes', labelsize=13)  # fontsize of the x and y labels
+# plt.rc('xtick', labelsize=16)  # fontsize of the x tick labels
+# plt.rc('ytick', labelsize=16)  # fontsize of the y tick labels
+# plt.rc('legend', fontsize=16)  # fontsize of the legend
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+file_loc = "../data/simulation_results/"
+file_name = "20230623_23_26_26_vacuum_V_fingerL_release"
+# file_name = "20230624_00_29_51_vacuum_V_fingerL_pullin"
+
+now = datetime.now()
+name_clarifier = "_edited_" + file_name
+timestamp = now.strftime("%Y%m%d_%H_%M_%S") + name_clarifier
+print(timestamp)
+
+colors = list(mcolors.TABLEAU_COLORS.keys())
+print(colors)
+
+data = np.load(file_loc + file_name + ".npy", allow_pickle=True)
+save_data_vacuum, save_data_air = data
+fingerL_values = sorted(save_data_vacuum.keys())
+print(fingerL_values)
+
+nx, ny = 3, 3
+fig, axs = plt.subplots(nx, ny)
+for i in range(len(fingerL_values)):
+    fingerL = fingerL_values[i]
+    V_converged, times_converged = save_data_vacuum[fingerL]
+    ax = axs[i // ny, i % ny]
+    line_vacuum, = ax.plot(V_converged, times_converged, colors[0])
+
+    V_converged, times_converged = save_data_air[fingerL]
+    line_air, = ax.plot(V_converged, times_converged, colors[1])
+
+    label = r"L=%0.1f$\mu$m" % (fingerL_values[i - 1] * 1e6)
+    ax.annotate(label, xy=(1, 1), xycoords='axes fraction', fontsize=10, xytext=(-2, -2), textcoords='offset points',
+                ha='right', va='top')
+
+axs[2, 1].set_xlabel("Voltage (V)", )
+axs[1, 0].set_ylabel(r"Time ($\mu$s)")
+
+if "pullin" in file_name:
+    fig.legend([line_vacuum], ['Pull-in Time, Vacuum'], loc='lower left')
+    fig.legend([line_air], ['Pull-in Time, Air'], loc='lower right')
+else:
+    fig.legend([line_vacuum], ['Release Time, Vacuum'], loc='lower left')
+    fig.legend([line_air], ['Release Time, Air'], loc='lower right')
+
+plt.tight_layout()
+plt.savefig("../figures/" + timestamp + ".png")
+plt.savefig("../figures/" + timestamp + ".svg")
+plt.savefig("../figures/" + timestamp + ".pdf")
+plt.show()
--- scripts/sim_gca_V_fingerL_pullin_release_vacuum.py
+++ scripts/sim_gca_V_fingerL_pullin_release_vacuum.py
@@ -5,9 +5,11 @@ Simulates the effect of varying finger overlap length and voltage on the pull-in
 was to model the dynamics when there's no damping term to mess up switch bounce, release behavior, and the like.
 """
 import os
-file_location = os.path.abspath(os.path.dirname( __file__))
+
+file_location = os.path.abspath(os.path.dirname(__file__))
 dir_location = os.path.abspath(os.path.join(file_location, '..'))
 import sys
+
 sys.path.append(file_location)
 sys.path.append(dir_location)
 
@@ -20,6 +22,7 @@ from scipy.io import loadmat, savemat
 from datetime import datetime
 from sklearn.metrics import r2_score, mean_squared_error
 import time
+
 np.set_printoptions(precision=3, suppress=True)
 
 
@@ -51,7 +54,7 @@ def sim_gca(model, u, t_span, verbose=False):
     terminate_simulation.terminal = True
 
     sol = solve_ivp(f, t_span, x0, events=[terminate_simulation], dense_output=True,
-                    max_step=0.1e-6)  # , method="LSODA")
+                    max_step=0.01e-6)  # , method="LSODA")
     return sol
 
 
@@ -71,7 +74,7 @@ def plot_data(fig, axs, pullin_V, pullin_avg, pullin_std, release_V, release_avg
         for idy in range(ny):
             # i = nx*idy + idx
             # ax = axs[idy, idx]
-            i = ny*idx + idy
+            i = ny * idx + idy
             # print(idx, idy, i)  # Confirms that I've ordered the data plots correctly
             ax = axs[idx, idy]
             ax.errorbar(pullin_V[i], pullin_avg[i], pullin_std[i], fmt='b.', capsize=3)
@@ -82,17 +85,20 @@ def plot_data(fig, axs, pullin_V, pullin_avg, pullin_std, release_V, release_avg
 
 
 if __name__ == "__main__":
+    pullin = True
+
     now = datetime.now()
     name_clarifier = "_vacuum_V_fingerL"
+    name_clarifier += "_pullin" if pullin else "_release"
     timestamp = now.strftime("%Y%m%d_%H_%M_%S") + name_clarifier
     print(timestamp)
 
-    t_span = [0, 200e-6]
+    t_span = [0, 300e-6]
     Fext = 0
     nx, ny = 3, 3
 
     data = loadmat("../data/20180208_fawn_gca_V_fingerL_pullin_release.mat")
-    fingerL_values = np.ndarray.flatten(data["LARR"])*1e-6  # Length = 9
+    fingerL_values = np.ndarray.flatten(data["LARR"]) * 1e-6  # Length = 9
 
     # latexify(fig_width=6, columns=3)
     fig, axs = setup_plot(nx, ny, x_label="Voltage (V)", y_label="Pull-in Time (us)")
@@ -110,6 +116,8 @@ if __name__ == "__main__":
     r2_scores_release = []
     rmse_pullin = []
     rmse_release = []
+    save_data_vacuum = {fingerL: () for fingerL in fingerL_values}
+    save_data_air = {fingerL: () for fingerL in fingerL_values}
     for i in range(1, len(fingerL_values) + 1):
         pullin_V.append(np.ndarray.flatten(data["V{}_Arr1".format(i)]))
         pullin_avg.append(np.ndarray.flatten(data["tmeas{}_Arr1".format(i)]))
@@ -117,56 +125,57 @@ if __name__ == "__main__":
         release_V.append(np.ndarray.flatten(data["V{}_Arr1_r".format(i)]))
         release_avg.append(np.ndarray.flatten(data["tmeas{}_Arr1_r".format(i)]))
         release_std.append(np.ndarray.flatten(data["err{}_Arr1_r".format(i)]))
-        labels.append(r"L=%0.1f$\mu$m"%(fingerL_values[i - 1]*1e6))
+        labels.append(r"L=%0.1f$\mu$m" % (fingerL_values[i - 1] * 1e6))
 
     # plot_data(fig, axs, pullin_V, pullin_avg, pullin_std, release_V, release_avg, release_std, labels)
     for idx in range(nx):
         for idy in range(ny):
-            i = ny*idx + idy
+            i = ny * idx + idy
             ax = axs[idx, idy]
             ax.annotate(labels[i], xy=(1, 1), xycoords='axes fraction', fontsize=10,
                         xytext=(-2, -2), textcoords='offset points',
                         ha='right', va='top')
 
-    legend_pullin, legend_release = None, None
+    legend_vacuum, legend_air = None, None
 
-    # Pullin measurements
+    # Vacuum
     for idy in range(len(fingerL_values)):
         fingerL = fingerL_values[idy]
         V_converged = []
         times_converged = []
 
-        V_test = np.arange(20, 91, 5)
+        V_test = np.arange(20, 90.1, 0.5)
+        # V_test = np.arange(20, 90.1, 0.25) if idy != 0 else np.arange(20, 90.05, 0.05)
         for V in V_test:
             start_time = time.process_time()
-            # Pullin
-            # model = setup_model_pullin(process=SOIvacuum())
-            # model.gca.fingerL = fingerL - model.gca.process.undercut
-            # model.gca.update_dependent_variables()
-            # model.gca.x0 = model.gca.x0_pullin()
-            # u = setup_inputs(V=V, Fext=Fext)
-
-            # Release
-            model = setup_model_release(V=V, Fext=Fext, process=SOIvacuum())
-            model.gca.fingerL = fingerL - model.gca.process.undercut
-            model.gca.update_dependent_variables()
-            u = [V, Fext]
-            model.gca.x0 = model.gca.x0_release(u)
-            u = setup_inputs(V=0, Fext=Fext)  # Changed for release
+            if pullin:  # Pullin
+                model = setup_model_pullin(process=SOIvacuum())
+                model.gca.fingerL = fingerL - model.gca.process.undercut
+                model.gca.update_dependent_variables()
+                model.gca.x0 = model.gca.x0_pullin()
+                u = setup_inputs(V=V, Fext=Fext)
+            else:  # Release
+                model = setup_model_release(V=V, Fext=Fext, process=SOIvacuum())
+                model.gca.fingerL = fingerL - model.gca.process.undercut
+                model.gca.update_dependent_variables()
+                u = [V, Fext]
+                model.gca.x0 = model.gca.x0_release(u)
+                u = setup_inputs(V=0, Fext=Fext)  # Changed for release
 
             sol = sim_gca(model, u, t_span)
 
             if len(sol.t_events[0]) > 0:
                 V_converged.append(V)
-                times_converged.append(sol.t_events[0][0]*1e6)  # us conversion
+                times_converged.append(sol.t_events[0][0] * 1e6)  # us conversion
 
             end_time = time.process_time()
-            print("Runtime for L=", fingerL, ", V=", V, "=", end_time - start_time)
+            print("Runtime for L=", fingerL, ", V=", V, "=", end_time - start_time, "->", times_converged)
         print(fingerL, V_converged, times_converged)
 
-        line, = axs[idy//ny, idy%ny].plot(V_converged, times_converged)
+        line, = axs[idy // ny, idy % ny].plot(V_converged, times_converged)
+        save_data_vacuum[fingerL] = (V_converged, times_converged)
         if idy == ny - 1:
-            legend_pullin = line
+            legend_vacuum = line
 
         # Calculate the r2 score
         # actual = []
@@ -185,43 +194,43 @@ if __name__ == "__main__":
         # rmse_pullin.append(rmse)
         # print("RMSE score for L=", fingerL, "=", rmse)
 
-    # Release measurements
+    # Air
     for idy in range(len(fingerL_values)):
         fingerL = fingerL_values[idy]
 
         V_converged = []
         times_converged = []
 
-        V_test = np.arange(20, 91, 5)
+        V_test = np.arange(20, 90.1, 0.5)
         for V in V_test:
             start_time = time.process_time()
-            # Pullin
-            # model = setup_model_pullin(process=SOI())
-            # model.gca.fingerL = fingerL - model.gca.process.undercut
-            # model.gca.update_dependent_variables()
-            # model.gca.x0 = model.gca.x0_pullin()
-            # u = setup_inputs(V=V, Fext=Fext)
-
-            # Release
-            model = setup_model_release(V=V, Fext=Fext, process=SOI())
-            model.gca.fingerL = fingerL - model.gca.process.undercut
-            model.gca.update_dependent_variables()
-            u = [V, Fext]
-            model.gca.x0 = model.gca.x0_release(u)
-            u = setup_inputs(V=0, Fext=Fext)  # Changed for release
+            if pullin:  # Pullin
+                model = setup_model_pullin(process=SOI())
+                model.gca.fingerL = fingerL - model.gca.process.undercut
+                model.gca.update_dependent_variables()
+                model.gca.x0 = model.gca.x0_pullin()
+                u = setup_inputs(V=V, Fext=Fext)
+            else:  # Release
+                model = setup_model_release(V=V, Fext=Fext, process=SOI())
+                model.gca.fingerL = fingerL - model.gca.process.undercut
+                model.gca.update_dependent_variables()
+                u = [V, Fext]
+                model.gca.x0 = model.gca.x0_release(u)
+                u = setup_inputs(V=0, Fext=Fext)  # Changed for release
 
             sol = sim_gca(model, u, t_span)
             if len(sol.t_events[0]) > 0:
                 V_converged.append(V)
-                times_converged.append(sol.t_events[0][0]*1e6)  # us conversion
+                times_converged.append(sol.t_events[0][0] * 1e6)  # us conversion
 
             end_time = time.process_time()
-            print("Runtime for L=", fingerL, ", V=", V, "=", end_time - start_time)
+            print("Runtime for L=", fingerL, ", V=", V, "=", end_time - start_time, "->", times_converged)
         print(times_converged)
 
-        line, = axs[idy//ny, idy%ny].plot(V_converged, times_converged, 'r')
+        line, = axs[idy // ny, idy % ny].plot(V_converged, times_converged, 'r')
+        save_data_air[fingerL] = (V_converged, times_converged)
         if idy == ny - 1:
-            legend_release = line
+            legend_air = line
 
         # Calculate the r2 score
         # actual = []
@@ -246,11 +255,16 @@ if __name__ == "__main__":
     # print("Pullin RMSE scores:", rmse_pullin, np.mean(rmse_pullin), np.std(rmse_pullin))
     # print("Release RMSE scores:", rmse_release, np.mean(rmse_release), np.std(rmse_release))
 
-    # axs[0, ny-1].legend([legend_pullin, legend_release], ['Pull-in', 'Release'])
-    # fig.legend([legend_pullin, legend_release], ['Pull-in in Vacuum', 'Pull-in in Air'], loc='lower right', ncol=2)
-    fig.legend([legend_pullin, legend_release], ['Release in Vacuum', 'Release in Air'], loc='lower right', ncol=2)
+    # axs[0, ny-1].legend([legend_vacuum, legend_air], ['Pull-in', 'Release'])
+    if pullin:
+        fig.legend([legend_vacuum, legend_air], ['Pull-in Time, Vacuum', 'Pull-in Time, Air'], loc='lower right',
+                   ncol=2)
+    else:
+        fig.legend([legend_vacuum, legend_air], ['Release Time, Vacuum', 'Release Time, Air'], loc='lower right',
+                   ncol=2)
 
     plt.tight_layout()
     plt.savefig("../figures/" + timestamp + ".png")
     plt.savefig("../figures/" + timestamp + ".pdf")
+    np.save("../data/simulation_results/" + timestamp + ".npy", (save_data_vacuum, save_data_air))
     plt.show()
